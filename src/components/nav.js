e6bf84b022b7d818a0a882fc58ba09d5298a0a64
justin <tan4@ualberta.ca>
2020-12-12 03:21:12 -0700

    Update files

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Resume from '../documents/resume.pdf'
+import Resume from '../../documents/resume.pdf'
 
 class Nav extends Component {
     render() {

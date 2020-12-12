cc12b6c295bbb6dc33c553bc464002f7e7b7f700
justin <tan4@ualberta.ca>
2020-12-12 03:20:08 -0700

    Update files

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Resume from '../documents/resume.pdf'
 
 class Nav extends Component {
     render() {
         return (
             <div className="info">
                 <h3 className="link">
-                    <a class="links hover" href="documents/resume.pdf"> {">"} resume</a>
+                    <a class="links hover" href={Resume}> {">"} resume</a>
                 </h3>
             </div>
         );
